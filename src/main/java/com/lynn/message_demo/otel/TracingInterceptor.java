c82601719e631c5690a75bd65ca5f21e3641fe26
Lynn <lynn990908@gmail.com>
2025-01-02 12:52:57 +0800

    [add] record error

--- src/main/java/com/lynn/message_demo/otel/TracingInterceptor.java
+++ src/main/java/com/lynn/message_demo/otel/TracingInterceptor.java
@@ -2,6 +2,7 @@ package com.lynn.message_demo.otel;
 
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.StatusCode;
 import io.opentelemetry.api.trace.Tracer;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -30,6 +31,9 @@ public class TracingInterceptor implements HandlerInterceptor {
   @Override
   public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
     Span span = (Span) request.getAttribute("span");
+    if (ex != null) {
+      span.recordException(ex).setStatus(StatusCode.ERROR,"request method %s failed");
+    }
     if (span != null) {
       span.end();
     }
